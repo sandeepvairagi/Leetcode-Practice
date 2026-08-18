b2a7635d06436d75bf1eaabf2586b7db5ab97d17
Sandeep vairagi <sandeepvairagi818@gmail.com>
2026-08-18 18:35:28 +0530

    Time: 1 ms (69.97%), Space: 43.3 MB (93.5%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        if (root == null) {
+            return ans;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode curr = q.poll();
+
+                // Current level ka last node
+                if (i == size - 1) {
+                    ans.add(curr.val);
+                }
+
+                if (curr.left != null) {
+                    q.offer(curr.left);
+                }
+
+                if (curr.right != null) {
+                    q.offer(curr.right);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
